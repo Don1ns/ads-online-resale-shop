dd4a292b175a2795805596fa2afba6a2d4e819bc
Riyaz Karimullin <riaz.karimullin@yandex.ru>
2023-05-27 18:44:57 +0300

    Initial commit

---
+++ src/main/java/me/don1ns/adsonlineresaleshop/AdsOnlineResaleShopApplication.java
@@ -0,0 +1,13 @@
+package me.don1ns.adsonlineresaleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsOnlineResaleShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsOnlineResaleShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/don1ns/adsonlineresaleshop/AdsOnlineResaleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.don1ns.adsonlineresaleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsOnlineResaleShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
