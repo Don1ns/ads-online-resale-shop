ff6f755cd64a4e9546c7d596623bc9a572f8ca11
sillyEjevika <Doroteia_Vika@mail.ru>
2023-05-27 20:51:07 +0300

    Созданы сущности Ads, AdsComment, User

--- src/main/java/me/don1ns/adsonlineresaleshop/entity/AdsComment.java
+++ src/main/java/me/don1ns/adsonlineresaleshop/entity/AdsComment.java
@@ -1,4 +1,26 @@
 package me.don1ns.adsonlineresaleshop.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Loginova Viktoria (Логинова Виктория)
+ **/
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class AdsComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private User user;
+    @ManyToOne
+    private Ads ads;
+    private LocalDateTime createdAt;
+    private String text;
 }
--- src/main/java/me/don1ns/adsonlineresaleshop/entity/User.java
+++ src/main/java/me/don1ns/adsonlineresaleshop/entity/User.java
@@ -1,4 +1,25 @@
 package me.don1ns.adsonlineresaleshop.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/**
+ * @author Loginova Viktoria (Логинова Виктория)
+ **/
+@Entity
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String image;
+    private String contentType;
 }
