06d5d9391a85d975ef705464448633b7816dffc8
Elena Nikitina <elena_nikitina_86@inbox.ru>
2023-05-27 21:12:47 +0300

    Первые два контроллера

--- src/main/java/me/don1ns/adsonlineresaleshop/controller/AdsController.java
+++ src/main/java/me/don1ns/adsonlineresaleshop/controller/AdsController.java
@@ -1,4 +1,24 @@
 package me.don1ns.adsonlineresaleshop.controller;
 
+import me.don1ns.adsonlineresaleshop.DTO.AdsDTO;
+import me.don1ns.adsonlineresaleshop.DTO.CreateAdsDTO;
+import me.don1ns.adsonlineresaleshop.DTO.ResponseWrapperAds;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ads")
+@CrossOrigin(value = "http://localhost:3000")
 public class AdsController {
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapperAds> getAllAds() {
+        return ResponseEntity.ok(new ResponseWrapperAds());
+    }
+
+    @PostMapping
+    public ResponseEntity<AdsDTO> addAds(@RequestBody CreateAdsDTO createAds) {
+        return ResponseEntity.ok(new AdsDTO());
+    }
+
 }
