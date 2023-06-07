2fe27b15b19d3a00bf3ed097d2c12b615f70338d
sillyEjevika <Doroteia_Vika@mail.ru>
2023-06-07 15:07:27 +0300

    сделан UserService

--- src/main/java/me/don1ns/adsonlineresaleshop/service/UserService.java
+++ src/main/java/me/don1ns/adsonlineresaleshop/service/UserService.java
@@ -1,13 +1,21 @@
 package me.don1ns.adsonlineresaleshop.service;
 
+import me.don1ns.adsonlineresaleshop.DTO.*;
+import me.don1ns.adsonlineresaleshop.entity.Image;
 import me.don1ns.adsonlineresaleshop.entity.User;
 
 public interface UserService {
-    void save(User user);
+    void createUser (RegisterReqDTO registerReqDto, Role role);
+    UserDTO setUserPassword(User authUser, NewPasswordDTO newPasswordDto);
+    User getUser(String username);
+    User updateUser(User authUser, UserDTO userDto);
+    UserDTO updateUserImage(User authUser, Image image);
+    UserDTO getCurrentUser(LoginReqDTO loginReqDTO);
 
+
+   /* void save(User user);
     void update(User user);
-
     void delete(User user);
     void deleteById(int id);
-    User getById(int id);
+    User getById(int id);*/
 }
