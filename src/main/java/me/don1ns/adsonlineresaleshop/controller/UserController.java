7b31e357ae41547ae021bd5f403bf86fabb18feb
Elena Nikitina <elena_nikitina_86@inbox.ru>
2023-06-09 14:34:08 +0300

    Правки по UserController.

--- src/main/java/me/don1ns/adsonlineresaleshop/controller/UserController.java
+++ src/main/java/me/don1ns/adsonlineresaleshop/controller/UserController.java
@@ -1,7 +1,10 @@
 package me.don1ns.adsonlineresaleshop.controller;
-
-import me.don1ns.adsonlineresaleshop.DTO.*;
+import lombok.RequiredArgsConstructor;
+import me.don1ns.adsonlineresaleshop.DTO.NewPasswordDTO;
+import me.don1ns.adsonlineresaleshop.DTO.UserDTO;
 import me.don1ns.adsonlineresaleshop.entity.User;
+import me.don1ns.adsonlineresaleshop.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,34 +12,57 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/users")
 @CrossOrigin(value = "http://localhost:3000")
+@RequiredArgsConstructor
 public class UserController {
 
-    // Обновление пароля
+    private final UserService userService;
+    private final User userDetails;
 
+    // Обновление пароля
     @PostMapping("/set_password")
-    public ResponseEntity<NewPasswordDTO> setPassword(@RequestBody NewPasswordDTO newPassword) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<UserDTO> setPassword(@RequestBody NewPasswordDTO newPassword) {
+        UserDTO userDTO = userService.setUserPassword(userDetails, newPassword);
+        if (userDTO != null) {
+            return ResponseEntity.ok().build();
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     // Получить информацию об авторизованном пользователе
-
     @GetMapping("/me")
-    public ResponseEntity<UserDTO> getUser() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<User> getUser() {
+        User user = userService.getUser(userDetails.getUsername());
+        if (user != null) {
+            return ResponseEntity.ok().build();
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     // Обновить информацию об авторизованном пользователе
-
     @PatchMapping("/me")
-    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO user) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<User> updateUser(@RequestBody UserDTO userDTO) {
+        User user = userService.updateUser(userDetails, userDTO);
+        if (user != null) {
+            return ResponseEntity.ok().build();
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     // Обновить аватар авторизованного пользователя
-
     @PatchMapping("/me/image")
-    public ResponseEntity<UserDTO> updateUserImage(@RequestParam MultipartFile image) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<UserDTO> updateUserImage(@RequestPart(name = "image") MultipartFile image) {
+        UserDTO userDTO = userService.updateUserImage(userDetails, image);
+        if (userDTO != null) {
+            return ResponseEntity.ok().build();
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
-
 }
