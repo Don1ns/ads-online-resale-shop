e684b37e4a6202be2e32dfe5be762bb75b9036f5
sillyEjevika <Doroteia_Vika@mail.ru>
2023-06-10 13:44:07 +0300

    переписан UserService , сделан UserServiceImpl (

--- src/main/java/me/don1ns/adsonlineresaleshop/service/impl/UserServiceImpl.java
+++ src/main/java/me/don1ns/adsonlineresaleshop/service/impl/UserServiceImpl.java
@@ -1,53 +1,78 @@
 package me.don1ns.adsonlineresaleshop.service.impl;
 
+import me.don1ns.adsonlineresaleshop.DTO.*;
+import me.don1ns.adsonlineresaleshop.entity.Image;
 import me.don1ns.adsonlineresaleshop.entity.User;
-import me.don1ns.adsonlineresaleshop.exception.ErrorMessage;
 import me.don1ns.adsonlineresaleshop.exception.UserNotFoundException;
+import me.don1ns.adsonlineresaleshop.mapper.UserMapper;
 import me.don1ns.adsonlineresaleshop.repository.UserRepository;
+import me.don1ns.adsonlineresaleshop.security.MyUserDetails;
 import me.don1ns.adsonlineresaleshop.service.UserService;
-import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import static me.don1ns.adsonlineresaleshop.constant.Constant.USER_NOT_FOUND_MSG;
+import java.io.File;
+import java.io.IOException;
 
-@Service
 public class UserServiceImpl implements UserService {
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+     private final UserMapper userMapper;
 
-    public UserServiceImpl(UserRepository repository) {
-        this.repository = repository;
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+
+    @Override
+    public UserDTO setPassword(NewPasswordDTO newPasswordDto, MyUserDetails currentUser) {
+        User user = checkUserByUsername(currentUser.getUsername());
+        userRepository.save(user);
+        return userMapper.toDto(user);
     }
 
     @Override
-    public void save(User user) {
-        repository.save(user);
+    public UserDTO getUser(MyUserDetails currentUser) {
+        User user = userRepository.findByUsername(currentUser.getUsername());
+        if (user != null) {
+            return userMapper.toDto(user);
+        } else {
+            return null;
+        }
     }
 
     @Override
-    public void update(User user) {
-        repository.save(user);
-    }
-
-    @Override
-    public void delete(User user) {
-        repository.delete(user);
-    }
-
-    @Override
-    public void deleteById(int id) {
-        repository.deleteById(id);
-    }
-
-    @Override
-    public User getById(int id) {
-        return repository.findById(id).orElseThrow(ErrorMessage::new);
+    public boolean updateUser(User userDto, MyUserDetails currentUser) {
+        User user = checkUserByUsername(currentUser.getUsername());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setPhone(userDto.getPhone());
+        userRepository.save(user);
+        return true;
     }
 
     @Override
     public User checkUserByUsername(String username) {
-        User user = repository.findByUsername(username);
+        User user = userRepository.findByUsername(username);
         if (user == null) {
-            throw new UserNotFoundException(USER_NOT_FOUND_MSG.formatted(username));
+            throw new UserNotFoundException(toString());
         }
+        return user;
+    }
+
+    @Override
+    public User updateUserImage(MultipartFile image, MyUserDetails currentUser) throws IOException {
+        User user = userRepository.findByUsername(currentUser.getUsername());
+        // Save the image to the server
+        String fileName = image.getOriginalFilename();
+        String fileExtension = fileName.substring(fileName.lastIndexOf("."));
+        String filePath = "path/to/image/directory/" + user.getId() + fileExtension; // Update the file path as per your server setup
+        File file = new File(filePath);
+        image.transferTo(file);
+
+        // Update the user's image URL
+        user.setImage((Image) image);
+        userRepository.save(user);
+
         return user;
     }
 }
